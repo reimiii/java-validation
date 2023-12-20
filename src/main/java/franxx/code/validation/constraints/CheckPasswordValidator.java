44c6986e40baaf6229c9f521d2bb207535d2b14e
Hilmi AM <imiia75775@proton.me>
2023-12-20 13:07:12 +0700

    Constraint Validator Context

--- src/main/java/franxx/code/validation/constraints/CheckPasswordValidator.java
+++ src/main/java/franxx/code/validation/constraints/CheckPasswordValidator.java
@@ -5,12 +5,35 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class CheckPasswordValidator implements ConstraintValidator<CheckPassword, Register> {
+
+    private String messageTemplate;
+
+    @Override
+    public void initialize(CheckPassword constraintAnnotation) {
+        this.messageTemplate = constraintAnnotation.message();
+    }
+
     @Override
     public boolean isValid(Register value, ConstraintValidatorContext context) {
         if ((value.getPassword() == null) || (value.getReTypePassword() == null) || (value.getUsername() == null)) {
             return true; // skip validation
         }
 
-        return value.getPassword().equals(value.getReTypePassword());
+        boolean isValid = value.getPassword().equals(value.getReTypePassword());
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+
+            context.buildConstraintViolationWithTemplate(messageTemplate)
+                    .addPropertyNode("password")
+                    .addConstraintViolation();
+
+
+            context.buildConstraintViolationWithTemplate(messageTemplate)
+                    .addPropertyNode("reTypePassword")
+                    .addConstraintViolation();
+        }
+
+        return isValid;
     }
 }
